2739b15c9780fd1434e5fa79f07938107045eaef
Mikhail Sotnikov <mialsot@gmail.com>
2024-05-18 05:11:06 +0600

    (internal) eslint setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        'airbnb',
+        'airbnb/hooks',
+    ],
+    rules: {
+        indent: ['error', 4],
+        'react/jsx-indent': ['error', 4],
+    },
+};
