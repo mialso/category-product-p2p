f2a4bc9f009e58af9906ab8bc6757d4e7dd27d9e
Mikhail Sotnikov <mialsot@gmail.com>
2024-05-18 04:40:49 +0600

    (internal) setup wepback and typescript, simple build

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// @ts-ignore
+document.querySelector('h1').addEventListener('click', (e) => { e.target.innerHTML = '🍐'})
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+
+export default (env) => ({
+  target: 'es2020',
+  entry: './src/index.ts',
+  mode: env.prod ? 'production' : 'development',
+  output: {
+    path: path.resolve('./', 'build'),
+    chunkFormat: 'module',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+});
+
